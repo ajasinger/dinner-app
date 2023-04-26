05affd5811070646ae7cdc88d903f8ada318d046
Aja Singer <ajasinger@gmail.com>
2023-04-26 17:47:36 -0400

    update HeaderBlock content

--- components/HeaderBlock.jsx
+++ components/HeaderBlock.jsx
@@ -3,8 +3,11 @@
 const HeaderBlock = () => {
     return (
         <div className="header-block">
-          <h1>Who's Coming to Dinner</h1>
-          <h2>Your AI enabled restaurant finder</h2>
+            <h1>Who's Coming to Dinner</h1>
+            <h2>
+                Your AI enabled restaurant finder &mdash; find the most convenient 
+                (and tasty) restaurant for you and your crew.
+            </h2>
         </div>
     );
 }
