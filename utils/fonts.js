9a5c92477829351a9dfa838054a09db6c6655c66
Aja Singer <ajasinger@gmail.com>
2023-04-27 16:19:24 -0400

    add fonts

---
+++ utils/fonts.js
@@ -0,0 +1,9 @@
+import { Sora, Space_Mono } from 'next/font/google';
+
+
+export const sora = Sora({ subsets: ['latin']});
+
+export const spaceMono = Space_Mono({
+  weight: ['400', '700'],
+  subsets: ['latin']
+});
