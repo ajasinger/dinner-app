fc7f28821f358ae8c18618afbf0807a06de4f8dd
Aja Singer <ajasinger@gmail.com>
2023-04-27 15:41:49 -0400

    add HeaderBlock and FormBlock to index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import HeaderBlock from '../components/HeaderBlock';
+import FormBlock from '../components/FormBlock';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,13 +12,15 @@ export default function Home() {
     <div>
       <Head>
         <title>Who's Coming to Dinner</title>
-        <meta name="description" content="Restaurant finder app" />
+        <meta name="description" content="AI-enabled restaurant finder app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        
+        < HeaderBlock />
+        < FormBlock />
+
       </main>
     </div>
   )
