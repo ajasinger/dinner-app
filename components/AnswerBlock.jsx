51e08ffeaafad9b2d24cc31cd5c819577cef51f0
Aja Singer <ajasinger@gmail.com>
2023-04-27 14:52:06 -0400

    update AnswerBlock layout

--- components/AnswerBlock.jsx
+++ components/AnswerBlock.jsx
@@ -1,9 +1,13 @@
 
 // define formResponse
+
 const AnswerBlock = ({formResponse}) => {
     return (
         <div className="answer-block">
+            <h2>Your restaurants</h2>
             <p>{formResponse.text}</p>
+            <button>Share list</button>
+            <button>Save list</button>
         </div>
     );
 }
