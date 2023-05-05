5fdd7aeaca2686f36cd071135d2a0fbd2ee7f408
Aja Singer <ajasinger@gmail.com>
2023-05-05 13:28:19 -0400

    add state dropdown to form

--- components/FormBlock.jsx
+++ components/FormBlock.jsx
@@ -7,6 +7,7 @@ const FormBlock = () => {
         address1: '',
         address2: '',
         adddress3: '',
+        state: '',
         eventType: {
             meeting: false,
             dateNight: false, 
@@ -38,6 +39,8 @@ const FormBlock = () => {
             ...formEntry,
             [e.target.name] : e.target.type === "checkbox" ? e.target.checked : e.target.value
         });
+
+        console.log(formEntry);
     }
 
     //event handler for submit
@@ -93,7 +96,67 @@ const FormBlock = () => {
                         onChange={handleChange}
                         className={spaceMono.className} 
                         placeholder="enter the neighborhood"
-                />
+                    />
+                    <label>
+                        State:
+                        <select 
+                            name="state"
+                            value={formEntry.state} 
+                            onChange={handleChange}
+                            className={spaceMono.className}
+                        >
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                            <option value="IA">Iowa</option>
+                            <option value="KS">Kansas</option>
+                            <option value="KY">Kentucky</option>
+                            <option value="LA">Louisiana</option>
+                            <option value="ME">Maine</option>
+                            <option value="MD">Maryland</option>
+                            <option value="MA">Massachusetts</option>
+                            <option value="MI">Michigan</option>
+                            <option value="MN">Minnesota</option>
+                            <option value="MS">Mississippi</option>
+                            <option value="MO">Missouri</option>
+                            <option value="MT">Montana</option>
+                            <option value="NE">Nebraska</option>
+                            <option value="NV">Nevada</option>
+                            <option value="NH">New Hampshire</option>
+                            <option value="NJ">New Jersey</option>
+                            <option value="NM">New Mexico</option>
+                            <option value="NY">New York</option>
+                            <option value="NC">North Carolina</option>
+                            <option value="ND">North Dakota</option>
+                            <option value="OH">Ohio</option>
+                            <option value="OK">Oklahoma</option>
+                            <option value="OR">Oregon</option>
+                            <option value="PA">Pennsylvania</option>
+                            <option value="RI">Rhode Island</option>
+                            <option value="SC">South Carolina</option>
+                            <option value="SD">South Dakota</option>
+                            <option value="TN">Tennessee</option>
+                            <option value="TX">Texas</option>
+                            <option value="UT">Utah</option>
+                            <option value="VT">Vermont</option>
+                            <option value="VA">Virginia</option>
+                            <option value="WA">Washington</option>
+                            <option value="WV">West Virginia</option>
+                            <option value="WI">Wisconsin</option>
+                            <option value="WY">Wyoming</option>
+                            </select>
+                    </label>
                 </fieldset>
 
                 {/* event type */}
