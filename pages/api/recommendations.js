3c5432e5b8d3e640c4ad402726915c7741903672
Aja Singer <ajasinger@gmail.com>
2023-05-08 10:47:02 -0400

    update API call

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -1,28 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
 
 //async funtion to fetch data 
 const fetchData = async () => {
   try {
-    const response = await fetch('https://api.openai.com/va/completions', {
-      method: 'POST',
-//    headers: {
-//      Authorization: `Bearer ${OPENAI_API_KEY}`,
-//      "Content-Type": "application/json"
-//    },
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${OPENAI_API_KEY}`,
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify({
         model: "text-davinci-003",
-        prompt: "",
+        prompt: "what is the best restaurant in New York?",
         //https://platform.openai.com/tokenizer
         max_tokens: 500,
-        temperature: 1,
+        temperature: 0,
       })
     })
     if(response.ok) {
       const openAiResponse = await response.json();
+      console.log(openAiResponse);
     }
     throw new Error('Request failed');
   } catch(error) {
