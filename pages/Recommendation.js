b1b6829db26265dffb960c3766584650ed54dadb
Aja Singer <ajasinger@gmail.com>
2023-05-10 14:46:22 -0400

    add AnswerBlock component

---
+++ pages/Recommendation.js
@@ -0,0 +1,9 @@
+import AnswerBlock from '../components/AnswerBlock';
+
+export default function Answer() {
+    return (
+        <div>
+            < AnswerBlock recommendations={recommendations} /> 
+        </div>
+    )
+  }
