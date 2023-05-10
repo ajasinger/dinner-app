9aa39dacb4ea4448783e7b7e8d2c0ff9c5b7d471
Aja Singer <ajasinger@gmail.com>
2023-05-10 14:53:17 -0400

    add HEaderBlock component to recommendations page

--- pages/Recommendation.js
+++ pages/Recommendation.js
@@ -3,6 +3,7 @@ import AnswerBlock from '../components/AnswerBlock';
 export default function Answer() {
     return (
         <div>
+            < HeaderBlock />
             < AnswerBlock recommendations={recommendations} /> 
         </div>
     )
