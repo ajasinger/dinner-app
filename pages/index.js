082788beceec79cfcead7dbd557b4f45a36aacd5
Aja Singer <ajasinger@gmail.com>
2023-05-10 14:57:34 -0400

    remove AnswerBlock component from homepage

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import HeaderBlock from '../components/HeaderBlock';
 import FormBlock from '../components/FormBlock';
-import AnswerBlock from '../components/AnswerBlock';
 
 export default function Home() {
   return (
@@ -18,9 +17,6 @@ export default function Home() {
       <main className={styles.main}>
         < HeaderBlock />
         < FormBlock />
-        {/* < AnswerBlock recommendations={recommendations} /> */}
-        
-
       </main>
     </div>
   )
