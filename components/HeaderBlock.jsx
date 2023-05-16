104f24aad933cb0dafdfa81fab9bc1140c0003e1
Aja Singer <ajasinger@gmail.com>
2023-05-16 15:00:06 -0400

    update emoji code and styles

--- components/HeaderBlock.jsx
+++ components/HeaderBlock.jsx
@@ -3,13 +3,13 @@ import styles from '@/styles/HeaderBlock.module.css'
 const HeaderBlock = () => {
     return (
         <div className={styles.header}>
-            <div className="ramen">&#1F35C</div>
-            <div className="taco">&#1F32E</div>
-            <div className="pizza">&#1F355</div>
-            <div className="sushi">&#1F359</div>
-            <div className="spaghetti">&#1F35D</div>
-            <div className="salad">&#1F957</div>
-            <div className="hamburger">&#1F354</div>
+            <div className={styles.ramen}>&#127836;</div>
+            <div className={styles.taco}>&#127790;</div>
+            <div className={styles.pizza}>&#127829;</div>
+            <div className={styles.sushi}>&#127833;</div>
+            <div className={styles.spaghetti}>&#127837;</div>
+            <div className={styles.salad}>&#129367;</div>
+            <div className={styles.hamburger}>&#127828;</div>
             <h1 className={styles.title}>Who's Coming to Dinner</h1>
             <h3>
                 Your AI enabled New York restaurant finder &mdash; find the most convenient 
