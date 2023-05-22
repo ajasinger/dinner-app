eca10c50240db74165756d0a76828b5fedc84047
Aja Singer <ajasinger@gmail.com>
2023-05-22 16:19:23 -0400

    remove placeholder props

--- pages/Recommendation.js
+++ pages/Recommendation.js
@@ -1,10 +1,11 @@
+import HeaderBlock from '../components/HeaderBlock';
 import AnswerBlock from '../components/AnswerBlock';
 
 export default function Answer() {
     return (
         <div>
             < HeaderBlock />
-            < AnswerBlock recommendations={recommendations} /> 
+            < AnswerBlock /> 
         </div>
     )
   }
