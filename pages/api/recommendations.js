32d5c3d9d93e2c3a4c6f383d72fb6f592e4b3662
Aja Singer <ajasinger@gmail.com>
2023-05-24 13:24:30 -0400

    import from openai and make variable for api key

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -1,8 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-// export default function handler(req, res) {
-//   res.status(200).json({ name: 'John Doe' })
-// }
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
 
 //async funtion to fetch data 
 const fetchData = async () => {
