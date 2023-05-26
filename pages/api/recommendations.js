bc3d73daf4d9839c7335d0a3eaba05f03465d4c8
Aja Singer <ajasinger@gmail.com>
2023-05-26 13:57:41 -0400

    update prompt to chatGPT

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -11,7 +11,13 @@ const openai = new OpenAIApi(configuration);
 const handler = async (req, res) => {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: "what is the best restaurant in New York?",
+    prompt: `what New York neighborhood is the center of these: ${addressString}?
+    Tell me the 5 best restaurants in hat neightborhoodt that would be a chosen by a food editor at the New York Times or Eater.
+    The restaurant should be good for this type of event: ${eventString}
+    The restaurant should serve this type of food: ${foodString}
+    The restaurant should have a price point that is: ${priceString}
+    Format the response as a numbered list containing: a) Restaurant name b) Restaurant address c) Restaurant neighborhood  d) Restaurant website e) if you can make reservations on Opentable or Resy
+    `,
     //https://platform.openai.com/tokenizer
     max_tokens: 500,
     temperature: 0,
