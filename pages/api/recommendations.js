bdb6e9e40a96e1d5c71e5701365fc2e991ad8e7c
Aja Singer <ajasinger@gmail.com>
2023-05-26 17:15:03 -0400

    try to handle POST request from form

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -9,22 +9,26 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const handler = async (req, res) => {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `what New York neighborhood is the center of these: ${addressString}?
-    Tell me the 5 best restaurants in hat neightborhoodt that would be a chosen by a food editor at the New York Times or Eater.
-    The restaurant should be good for this type of event: ${eventString}
-    The restaurant should serve this type of food: ${foodString}
-    The restaurant should have a price point that is: ${priceString}
-    Format the response as a numbered list containing: a) Restaurant name b) Restaurant address c) Restaurant neighborhood  d) Restaurant website e) if you can make reservations on Opentable or Resy
-    `,
-    //https://platform.openai.com/tokenizer
-    max_tokens: 500,
-    temperature: 0,
-  });
+  const body = req.query.result;
+  console.log('body: ', body);
+  res.status(200).json({data: `${body}`})
 
-  const recommendations = completion.data.choices[0].text;
-  res.status(200).json({ recommendations })
+  // const completion = await openai.createCompletion({
+  //   model: "text-davinci-003",
+  //   prompt: `what New York neighborhood is the center of these: ${addressString}?
+  //   Tell me the 5 best restaurants in hat neightborhoodt that would be a chosen by a food editor at the New York Times or Eater.
+  //   The restaurant should be good for this type of event: ${eventString}
+  //   The restaurant should serve this type of food: ${foodString}
+  //   The restaurant should have a price point that is: ${priceString}
+  //   Format the response as a numbered list containing: a) Restaurant name b) Restaurant address c) Restaurant neighborhood  d) Restaurant website e) if you can make reservations on Opentable or Resy
+  //   `,
+  //   //https://platform.openai.com/tokenizer
+  //   max_tokens: 500,
+  //   temperature: 0,
+  // });
+
+  // const recommendations = completion.data.choices[0].text;
+  // res.status(200).json({ recommendations })
 }
 
 export default handler;
