5d851232089abf5a2e15271d62819091a5998f52
Aja Singer <ajasinger@gmail.com>
2023-06-05 12:55:09 -0400

    define props for GoogleMapReact component

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,11 +1,23 @@
 import GoogleMapReact from "google-map-react";
 
 const Map = () => {
+    const coordinates = { lat:0, lng:0 }
+
     return(
         <div className={styles.mapContainer}>
-            <GoogleMapReact>
+            <GoogleMapReact
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={""}
+                onChange={""}
+                onChildClick={""}
+            >
 
             </GoogleMapReact>
         </div>
     )
 }
+
+export default Map;
