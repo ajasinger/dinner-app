c2cb30089840ef8acbb55c952fee4ec7e5a91bed
Aja Singer <ajasinger@gmail.com>
2023-06-09 14:01:21 -0400

    add className for text block

--- components/HeaderBlock.jsx
+++ components/HeaderBlock.jsx
@@ -4,7 +4,7 @@ const HeaderBlock = () => {
     return (
         <div className={styles.header}>
             {/* <h3 className={styles.login}>Login</h3> */}
-            <div className={styles.headerText}>
+            <div className={styles.headerContent}>
                 <div className={styles.ramen}>&#127836;</div>
                 <div className={styles.taco}>&#127790;</div>
                 <div className={styles.pizza}>&#127829;</div>
@@ -12,11 +12,13 @@ const HeaderBlock = () => {
                 <div className={styles.pancakes}>&#129374;</div>
                 <div className={styles.pita}>&#129369;</div>
                 <div className={styles.hamburger}>&#127828;</div>
-                <h1 className={styles.title}>Who&apos;s Coming to Dinner</h1>
-                <h3 className={styles.subtitle}>
-                    Your AI enabled New York restaurant finder &mdash; find the most convenient 
-                    &#40;and tasty&#41; restaurant for you and your crew.
-                </h3>
+                <div className={styles.headerText}>
+                    <h1 className={styles.title}>Who&apos;s Coming to Dinner</h1>
+                    <h3 className={styles.subtitle}>
+                        Your AI enabled New York restaurant finder &mdash; find the most convenient 
+                        &#40;and tasty&#41; restaurant for you and your crew.
+                    </h3>
+                </div>
             </div>
         </div>
     );
