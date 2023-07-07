44724f0f147305f6e76e9c78e19e2f2c25f2f7ab
Aja Singer <ajasinger@gmail.com>
2023-07-07 15:52:17 -0400

    new map component for data-driven-styling

---
+++ components/NeighborhoodMap.jsx
@@ -0,0 +1,11 @@
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+const NeighborhoodMap = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default NeighborhoodMap;
